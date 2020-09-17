ca692b6b7dd0bd36f6b6232f6096fd54f11825e9
noewal <noewal91@gmail.com>
2020-09-17 21:32:49 +0900

    Revert "修正２"
    This reverts commit 0a229b71307be6df236ae9202783c8bf38123ec2.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 6 }
   validates :password, format: { with: /\A[a-zA-Z0-9]+\z/, message: "is invalid. Input half-width characters."}
   validates :password, presence: true
-#   validates :password_confirmation, presence: true
+  validates :password_confirmation, presence: true
   validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
    validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
    validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
