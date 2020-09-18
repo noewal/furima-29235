8eb62dc1d8eea226d8b263253dfe9a428b253ecc
noewal <noewal91@gmail.com>
2020-09-18 18:19:52 +0900

    itemモデルのバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :day, :delivery_cost, :prefecture, :status
+
+  with_options presence: true do
+    validates :name,          length: { maximum: 40 }
+    validates :description,   length: { maximum: 1000 }
+    validates :price,         numericality: { :greater_than => 300 }, { :less_than_or_equal_to => 9999999 } 
+  end
+
+  with_options numericality: { other_than: 1 }
+    validates:category_id,        presence: true
+    validates:day_id,             presence: true
+    validates:delivery_cost_id,   presence: true
+    validates:prefecture_id,      presence: true
+    validates:status_id,          presence: true
+  end
+
   belongs_to :users
 end
