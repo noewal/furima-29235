3877680962b46a5b36dd8e777a04477c2b5966f0
noewal <noewal91@gmail.com>
2020-09-18 14:02:36 +0900

    複数回使用を変数化修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,17 @@ class User < ApplicationRecord
   devise  :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
   
+  zenkaku = /\A[ぁ-んァ-ン一-龥]/
+  kana = /\A[ァ-ヶー－]+\z/
+
   with_options presence: true do
     validates :nickname
     validates :password, length: { minimum: 6 },
               format: { with: /\A[a-zA-Z0-9]+\z/, message: "is invalid. Input half-width characters."}
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :family_name, format: { with: zenkaku, message: "is invalid. Input full-width characters."}
+    validates :first_name, format: { with: zenkaku, message: "is invalid. Input full-width characters."}
+    validates :family_name_kana, format: { with: kana, message: "is invalid. Input full-width katakana characters."}
+    validates :first_name_kana, format: { with: kana, message: "is invalid. Input full-width katakana characters."}
     validates :birthday
   
   end
