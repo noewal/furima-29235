0565f16295769fad2dc8c1fcb0284f25b523893b
noewal <noewal91@gmail.com>
2020-09-20 10:30:23 +0900

    javascriptで非同期的に販売手数料や販売利益の表示実装

---
+++ app/javascript/calculation.js
@@ -0,0 +1,17 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+  const price = document.getElementById("item-price");
+  const tax_price = document.getElementById("add-tax-price");
+  const profit_price = document.getElementById("profit");
+
+  price.addEventListener("input", () => {
+    let price_num = document.getElementById("item-price").value;
+    let tax_price_num;
+    console.log(price);
+
+    tax_price.innerHTML = price_num * 0.1;
+
+    tax_price_num = price_num * 0.1;
+    profit_price.innerHTML =  (price_num - tax_price_num);
+  });
+}); 
