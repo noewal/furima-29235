4a34ba391d629a28c5db622c44c11bffbcc7bfdd
noewal <noewal91@gmail.com>
2020-09-21 18:24:19 +0900

    destroyアクションが行われた後、商品一覧ページを表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,8 +30,11 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    item.destroy
-  end
+    if item.destroy
+      redirect_to root_path
+    else
+      render :show
+    end  end
 
   private
 
